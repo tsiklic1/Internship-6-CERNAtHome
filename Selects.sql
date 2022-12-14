2616b29dce4685c48c973243c9a290177ed95a03
Toma Siklic <toma.siklic@gmail.com>
2022-12-14 18:05:25 +0100

    adding third print

--- Selects.sql
+++ Selects.sql
@@ -9,3 +9,13 @@ FROM ResearchPapers rp
 --2
 SELECT s.FirstName, s.LastName, s.Sex, c.Name, c.PPPCapitaInCents FROM Scientists s
 JOIN Countries c ON c.CountryId = s.CountryId
+
+--3
+SELECT p.Name AS ProjectName,--, a.Name AS AcceleratorName
+CASE
+WHEN(ap.AcceleratorId IS NULL) THEN 'NEMA GA'
+ELSE a.Name END AS AcceleratorName
+FROM Projects p
+JOIN AcceleratorProjects ap ON p.ProjectId = ap.ProjectId
+LEFT JOIN Accelerators a ON a.AcceleratorId = ap.AcceleratorId
+
