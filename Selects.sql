1318666043b07033fae6ba38416177f62878be4c
Toma Siklic <toma.siklic@gmail.com>
2022-12-14 21:33:38 +0100

    added fourth print

--- Selects.sql
+++ Selects.sql
@@ -10,7 +10,7 @@ FROM ResearchPapers rp
 SELECT s.FirstName, s.LastName, s.Sex, c.Name, c.PPPCapitaInCents FROM Scientists s
 JOIN Countries c ON c.CountryId = s.CountryId
 
---3 NIJE DOBRO
+--3
 SELECT p.Name as ProjectName,
 CASE
 WHEN(a.Name IS NULL) THEN 'NEMA GA'
@@ -19,7 +19,10 @@ FROM Projects p
 LEFT JOIN AcceleratorProjects ap ON p.ProjectId = ap.ProjectId
 LEFT JOIN Accelerators a ON a.AcceleratorId = ap.AcceleratorId
 
-
+--4
+SELECT * FROM Projects p
+WHERE (SELECT COUNT(*) FROM ResearchPapers rp WHERE p.ProjectId = rp.ProjectId AND 
+	  DATE_PART('year', rp.PublishedAt) >= 2015 AND DATE_PART('year', rp.PublishedAt) <= 2017) > 0
 
 
 
